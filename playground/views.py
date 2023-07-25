f49895f0f22f07eaff543c2a901ad171920b6912
Ling Fung Chan <justinlfchan@gmail.com>
2023-07-25 14:22:44 +0100

    tried run and debug function inside vs code by adding breakpoint and checking code line by line

--- playground/views.py
+++ playground/views.py
@@ -15,9 +15,18 @@ from django.http import HttpResponse
 # * We need to map this view (request handler) to a url in 'urls.py'
 
 def say_hello(request):
+    # * This is for django application debug demonstration in VS Code
+    z = calculate()
+
     # return HttpResponse('Hello World')
 
     # 1st argument: The "request" itself
     # 2nd argument: The HTML file name
     # 3rd argument: The mapping object (which is dictionary in Python)
     return render(request, 'hello.html', {'name': 'lingfungc'})
+
+# * This is for django application debug demonstration in VS Code
+def calculate():
+    x = 1
+    y = 2
+    return x
